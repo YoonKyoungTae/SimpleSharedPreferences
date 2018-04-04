156be2628456eb6885069ccb4b352fde232cc65c
ryans <yoon91.dev@gmail.com>
2018-04-05 01:15:49 +0900

    Upgrade SSP.class (Base)

--- library/src/main/java/kr/co/diordna/simplesharedpreferences/SSP.java
+++ library/src/main/java/kr/co/diordna/simplesharedpreferences/SSP.java
@@ -1,8 +1,37 @@
 package kr.co.diordna.simplesharedpreferences;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 /**
  * Created by ryans on 2018-04-03.
  */
 
 public class SSP {
+
+    private SharedPreferences mPref;
+    private SharedPreferences.Editor mEditor;
+
+    private volatile static SSP instance = null;
+
+    private SSP(){}
+
+    public SSP getInstance() {
+        if (instance == null) {
+            synchronized (SSP.class) {
+                if (instance == null) {
+                    instance = new SSP();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    public void init(Context context) {
+        mPref = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        mEditor = mPref.edit();
+    }
+
 }
